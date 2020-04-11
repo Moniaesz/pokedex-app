253d82558b785fd654a383a3c0e5a8774a2cd3f8
Monika <molinskamonika+github@gmail.com>
2020-04-11 19:45:58 +0200

    cleanup & seting basic routing

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import pokemonPlaceholder from '../../assets/pokemon-placeholder.svg';
+import '../Header/Header.css';
+import { Link } from 'react-router-dom'
+
+
+const Header = () => (
+  <header className='header'>
+    <h1 className='app-name'>POKEDEX APP</h1>
+    <img src={pokemonPlaceholder} alt='pokedex app logo' className='app-logo'/>
+    <nav>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/pokemon-list'>Pokemon</Link>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
---
+++ src/components/PokemonList/PokemonList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PokemonList = () => {
+  return (
+    <h2>Pokemon List Page</h2>
+  )
+}
+
+export default PokemonList;
