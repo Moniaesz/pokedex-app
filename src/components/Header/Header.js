93833a1b865e11edcc89e7d84c3d32211a982f27
Monika <molinskamonika+github@gmail.com>
2020-04-12 12:23:33 +0200

    add Header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import pokemonPlaceholder from '../../assets/pokemon-placeholder.svg';
-import '../Header/Header.css';
+import './Header.css';
 import { Link } from 'react-router-dom'
 
 
 const Header = () => (
   <header className='header'>
-    <h1 className='app-name'>POKEDEX APP</h1>
-    <img src={pokemonPlaceholder} alt='pokedex app logo' className='app-logo'/>
-    <nav>
-      <ul>
-        <Link to='/'>Home</Link>
-        <Link to='/pokemon-page'>Pokemon</Link>
+    <div className='app-name'>
+      <h1 className='app-name__heading'>POKEDEX APP</h1>
+      <img src={pokemonPlaceholder} alt='pokedex app logo' className='app-logo'/>
+    </div>
+    <nav className='nav'>
+      <ul className='nav__list'>
+        <Link to='/' className='nav__link'>Home</Link>
+        <Link to='/pokemon-page' className='nav__link'>Pokemon</Link>
       </ul>
     </nav>
   </header>
