d4374f4a990e78db34b2e49e2e1bb995e436f3b0
Monika <molinskamonika+github@gmail.com>
2020-04-12 13:49:18 +0200

    add Home page styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './Home.css';
+import heroImg from '../../assets/hero-img.jpg';
+import pokemonAvatar from '../../assets/pokemon-avatar.svg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <h2>Home Page</h2>
+    <section
+      className='homepage'
+      style={{backgroundImage: `url(${heroImg})`}}
+    >
+      <div className='homepage__content'>
+        <h2>Welcome to POKEDEX</h2>
+        <img src={pokemonAvatar} alt='pokemon avatar'/>
+        <p className='homepage__content__description'>Browse through all Pokemon, find out details about each one and save your favourite ones!</p>
+        <Link to='/pokemon-page' className='find-pokemon__btn'>Find Pokemon</Link>
+      </div>
+    </section>
   )
 }
 
