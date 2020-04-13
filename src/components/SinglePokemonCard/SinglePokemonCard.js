f1b66b50c71849a2a807d16acedf06e0f077da9b
Monika <molinskamonika+github@gmail.com>
2020-04-13 15:36:53 +0200

    add toggle favourite pokemon icon to SinglePokemonCard

--- src/components/SinglePokemonCard/SinglePokemonCard.js
+++ src/components/SinglePokemonCard/SinglePokemonCard.js
@@ -1,13 +1,15 @@
 import React, { useContext, useEffect } from 'react';
 import { PokemonContext } from '../../contexts/PokemonContext';
 import pokemonPlaceholder from '../../assets/pokemon-placeholder.svg';
+import addTofavIcon from '../../assets/add_to_fav_icon.svg';
+import favIcon from '../../assets/fav_icon.svg';
 import { Link } from 'react-router-dom';
 import './SinglePokemonCard.css';
 import pokemonTypes from  '../../helpers/pokemon-types';
 
 const SinglePokemonCard = ({ name }) => {
 
-  const { pokemonCache, setPokemonCache } = useContext(PokemonContext);
+  const { pokemonCache, setPokemonCache, isPokemonFavourite, toggleFavourites } = useContext(PokemonContext);
 
   useEffect(() => {
     if (!pokemonCache[name]) {
@@ -27,13 +29,18 @@ const SinglePokemonCard = ({ name }) => {
 
   return (
     <li className='pokemons-names__item'>
+      <img
+          alt='add to favourites'
+          src={isPokemonFavourite(name) ? favIcon : addTofavIcon}
+          className='add-to-fav__img--list'
+          onClick={() => toggleFavourites(name)}
+        />
       <h3 className='single-pokemon__name'>{name}</h3>
       <img
         alt={name}
         src={pokemonCache[name] ? pokemonCache[name].sprites.front_default : pokemonPlaceholder}
         className='single-pokemon__img'
       />
-      {/* <h5>base experience: {pokemonCache[name] ? pokemonCache[name].base_experience : ''}</h5> */}
       {
         pokemonCache[name] &&
         <>
@@ -44,7 +51,6 @@ const SinglePokemonCard = ({ name }) => {
               const typeIcon = pokemonTypes.find((pokemon) => pokemon.type === type.name);
               return (
                 <li key={type.name}>
-                  {/* <h6>{type.name}</h6> */}
                   <img alt={type.name} src={typeIcon.url} className='type-icon'/>
                 </li>
               )
