10464ccb52b608d032f126d2a0d60acf12bc69d7
Monika <molinskamonika+github@gmail.com>
2020-04-14 10:19:37 +0200

    move data checking/fetching logic to reusable PokemonDetailsCard component

--- src/components/PokemonDetailsCard/PokemonDetailsCard.js
+++ src/components/PokemonDetailsCard/PokemonDetailsCard.js
@@ -1,6 +1,7 @@
-import React , { useContext, useState } from 'react';
+import React , { useContext, useState, useEffect } from 'react';
 import { PokemonContext } from '../../contexts/PokemonContext';
 import addTofavIcon from '../../assets/add_to_fav_icon.svg';
+import loadingIcon from '../../assets/loading.svg';
 import favIcon from '../../assets/fav_icon.svg';
 import pokemonTypes from '../../helpers/pokemon-types';
 import './PokemonDetailsCard.css';
@@ -8,47 +9,72 @@ import './PokemonDetailsCard.css';
 const PokemonDetailsCard = ({ pokemonName, add }) => {
 
   const [ isHovered, setIsHovered ] = useState(false);
-  const { pokemonCache, isPokemonFavourite,toggleFavourites } = useContext(PokemonContext);
+  const { pokemonCache, setPokemonCache, isPokemonFavourite,toggleFavourites } = useContext(PokemonContext);
+
+  useEffect(() => {
+    if (!pokemonCache[pokemonName]) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then(res => res.json())
+      .then(pokemonDescription => {
+        setPokemonCache((prevDetails) => ({
+            ...prevDetails,
+            [pokemonName]: pokemonDescription
+          }))
+        })
+        .catch((err) => {
+            console.log('fetching pokemon details error', err)
+        });
+    }
+  }, []);
 
   return (
     <div className='pokemon-details__card'>
-      <div className='add-to-fav__wrapper'>
+    { pokemonCache[pokemonName]
+      ? <>
+        <div className='add-to-fav__wrapper'>
+          <img
+            alt='add to favourites'
+            src={isPokemonFavourite(pokemonName) ? favIcon : addTofavIcon}
+            className='add-to-fav__img'
+            onMouseOver={() => setIsHovered(true)}
+            onMouseOut={() => setIsHovered(false)}
+            onClick={() => toggleFavourites(pokemonName)}
+          />
+          {
+            add && (
+              <div className={isHovered ? 'add-to-fav-tooltip hovered' : 'add-to-fav-tooltip'}>
+                <h5>{isPokemonFavourite(pokemonName) ? 'remove from favourites' : 'add to favourites'}</h5>
+              </div>
+            )
+          }
+        </div>
         <img
-          alt='add to favourites'
-          src={isPokemonFavourite(pokemonName) ? favIcon : addTofavIcon}
-          className='add-to-fav__img'
-          onMouseOver={() => setIsHovered(true)}
-          onMouseOut={() => setIsHovered(false)}
-          onClick={() => toggleFavourites(pokemonName)}
+          alt={pokemonCache[pokemonName].name}
+          src={pokemonCache[pokemonName].sprites.front_default} className='pokemon-details__img'
         />
-        {
-          add && (
-            <div className={isHovered ? 'add-to-fav-tooltip hovered' : 'add-to-fav-tooltip'}>
-              <h5>{isPokemonFavourite(pokemonName) ? 'remove from favourites' : 'add to favourites'}</h5>
-          </div>
-          )
-        }
-      </div>
-      <img
-        alt={pokemonCache[pokemonName].name}
-        src={pokemonCache[pokemonName].sprites.front_default} className='pokemon-details__img'
-      />
-      <h3 className='pokemon-details__name'>{pokemonCache[pokemonName].name}</h3>
-      <p>weight: {pokemonCache[pokemonName].weight}</p>
-      <p>height: {pokemonCache[pokemonName].height}</p>
-      <p>base experience: {pokemonCache[pokemonName].base_experience}</p>
-      <h5>type/s:</h5>
-      <ul className='pokemon-detail__types'>
-        {pokemonCache[pokemonName].types.map(({type}) => {
-          const typeIcon = pokemonTypes.find((pokemon) => pokemon.type === type.name);
-          return (
-            <li key={type.name}>
-              <h6>{type.name}</h6>
-              <img alt={type.name} src={typeIcon.url} className='type-icon'/>
-            </li>
-          )
-        })}
-    </ul>
+        <h3 className='pokemon-details__name'>{pokemonCache[pokemonName].name}</h3>
+        <p>weight: {pokemonCache[pokemonName].weight}</p>
+        <p>height: {pokemonCache[pokemonName].height}</p>
+        <p>base experience: {pokemonCache[pokemonName].base_experience}</p>
+        <h5>type/s:</h5>
+        <ul className='pokemon-detail__types'>
+          {pokemonCache[pokemonName].types.map(({type}) => {
+            const typeIcon = pokemonTypes.find((pokemon) => pokemon.type === type.name);
+            return (
+              <li key={type.name}>
+                <h6>{type.name}</h6>
+                <img alt={type.name} src={typeIcon.url} className='type-icon'/>
+              </li>
+            )
+          })}
+        </ul>
+      </>
+
+      : <div className='loading'>
+          <h4 className='loading__heading'>Loading Pokemons details...</h4>
+          <img alt='loading spinner' src={loadingIcon} className='loading-spinner'/>
+        </div>
+    }
   </div>
   )
 }
